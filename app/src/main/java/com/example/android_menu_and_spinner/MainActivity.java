75de7987af98d6c2cf5d3c35263220dee94d5ccb
Karan1811 <kumarkay.ca@gmail.com>
2019-11-25 19:16:33 -0500

    OnContextMenu

--- app/src/main/java/com/example/android_menu_and_spinner/MainActivity.java
+++ app/src/main/java/com/example/android_menu_and_spinner/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.android_menu_and_spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
+        Button menu_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        menu_btn=findViewById(R.id.button_menu);
+
+        menu_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerForContextMenu(menu_btn);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater mf=getMenuInflater();
+        mf.inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_add:
+            {
+                Toast.makeText(MainActivity.this,"ADD",Toast.LENGTH_LONG).show();
+
+                break;
+            }
+            case R.id.action_edit:
+            {
+                Toast.makeText(MainActivity.this,"EDIT",Toast.LENGTH_LONG).show();
+                break;
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater mf=getMenuInflater();
+        mf.inflate(R.menu.menu_main,menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add:
+            {
+                Toast.makeText(MainActivity.this,"ADD",Toast.LENGTH_LONG).show();
+
+                break;
+            }
+            case R.id.action_edit:
+            {
+                Toast.makeText(MainActivity.this,"EDIT",Toast.LENGTH_LONG).show();
+                break;
+            }
+
+        }
+        return super.onContextItemSelected(item);
     }
 }
